c04ba4db8914484f013040f00e049b46d39ffa33
Leo Hernandez <leo@leojh.com>
2015-07-07 12:52:56 -0400

    Allowed '.' as initial input. Better selected text support.

--- addon/components/ember-numeric-input.js
+++ addon/components/ember-numeric-input.js
@@ -3,18 +3,25 @@ import Ember from 'ember';
 export default Ember.TextField.extend({
   willInsertElement: function() {
     this.$().on("keypress", e => {
-      if(this.isInvalidValue(e)) {
+      if(!this.isValidValue(e)) {
         e.preventDefault();
       }
     });
   },
 
-
   numberValue: Ember.computed('value', {
     get() {
-      return Number(this.get('value'));
+      let value = this.get('value');
+
+      if (value === ".") {return 0;}
+
+      return Number(value);
     },
     set(key, value) {
+      if (this.get('value') === ".") {
+        value = ".";
+      }
+
       this.set('value', value ? value : null);
     }
   }),
@@ -22,20 +29,37 @@ export default Ember.TextField.extend({
   getNewValue: function(e) {
     let keyCode = e.keyCode || e.which;
     let newChar = String.fromCharCode(keyCode);
-    return Ember.$(e.currentTarget).val() + newChar;
+    let value = (this.get('value') || "").toString().replace(this.getSelectionText(), "");
+
+    return value + newChar;
   },
 
   moreThanTwoDecimals: function(e) {
     return this.getNewValue(e).countDecimals() > 2;
   },
 
-  isInvalidValue: function(e) {
-    if (e.altKey || e.metaKey || e.shiftKey || e.ctrlKey) { return false;}
-    if (e.which === 0) {return false;} //arrow keys, etc, in FF
-    if (e.which === 8) {return false;} //backspace
+  isNewValueAPeriod: function(e) {
+    return this.getNewValue(e) === ".";
+  },
+
+  isValidValue: function(e) {
+    if (this.isNewValueAPeriod(e)) { return true; }
+
+    if (e.altKey || e.metaKey || e.shiftKey || e.ctrlKey) { return true;}
+    if (e.which === 0) {return true;} //arrow keys, etc, in FF
+    if (e.which === 8) {return true;} //backspace
 
     let value = this.getNewValue(e);
 
-    return isNaN(value) || value.countDecimals() > 2;
+    return !isNaN(value) && value.countDecimals() <= 2;
+  },
+
+  getSelectionText: function() {
+    let element = this.$()[0];
+    if(element.tagName === "TEXTAREA" ||
+      (element.tagName === "INPUT" && element.type === "text")) {
+       return element.value.substring(element.selectionStart, element.selectionEnd);
+   }
+   return null;
   }
 });
